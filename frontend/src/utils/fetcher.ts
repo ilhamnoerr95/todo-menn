1b4ce0554acfe4f250487227fbab5d55ac883395
ilhamnrfdn <ilhamnoer@femaledaily.com>
2024-11-10 23:21:50 +0700

    add utils for fetching & ssr

--- frontend/src/utils/fetcher.ts
+++ frontend/src/utils/fetcher.ts
@@ -2,33 +2,48 @@ interface IFetcher {
 	method: "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
 	body?: any;
 	url?: string;
+	// queryKey default dari react query, jadi fungsi di dalam react query
+	// masih bisa digunakan tanpa melempar parameter di dalam fungsinya
 	queryKey?: string[];
 }
 
 export const DefaultFetcher = async (payload: IFetcher) => {
 	try {
+		console.log("ini default fetcher", payload);
 		// 2 optional jika payload url ada jika tidak ambil dari query keys
 		let URL: string = "";
 
 		if (payload.url) {
 			URL = payload.url;
 		} else {
-			const queryKeys = payload.queryKey ?? [];
-			URL = new URLSearchParams(payload.body).toString();
+			// const queryKeys = payload.queryKey ?? [];
+			// URL = new URLSearchParams(payload.body).toString();
+			const params = payload.queryKey?.[1]
+				? `?${new URLSearchParams({ ...(payload.queryKey?.[1] as any) })}`
+				: "";
+
+			URL = `${process.env.NEXT_PUBLIC_ROOT_FE}/api${payload.queryKey?.[0]}${params}`;
 		}
+
+		console.log({ URL, env: process.env.NEXT_PUBLIC_ROOT_FE });
 
 		const OPTION: RequestInit = {
 			method: payload.method,
 			headers: {
 				"Content-Type": "application/json",
+				version: payload.queryKey?.[2] || "v1",
 			},
+			...(payload.body && { body: JSON.stringify(payload.body) }),
 		};
+
 		const response = await fetch(URL, OPTION);
+		const result = await response.json();
+
 		if (!response.ok) {
 			Promise.reject(response);
 		}
 
-		await response.json();
+		return result;
 	} catch (error) {
 		Promise.reject(error);
 	}
---
+++ frontend/src/utils/ssrWrapper.ts
@@ -0,0 +1,31 @@
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { GetServerSidePropsContext } from "next";
+import { defaultQuery } from "@/hooks/default.queries";
+
+export const ssrWrapper = (ssr: (payload: any) => any, url?: string) => {
+	return async (context: GetServerSidePropsContext) => {
+		const { req, res, query } = context;
+
+		// default queries in ssr Wrapper
+		const queryClient = new QueryClient({
+			defaultOptions: {
+				queries: defaultQuery({ url }),
+			},
+		});
+
+		// clear cache in ssr agar memory tidak leaks
+		queryClient.clear();
+
+		// ssr callback yg mereturn si callbacnya dan parameter callback ini adalah
+		// value dari utils ini sendiri
+		const additional = await ssr({ test: "ss", queryClient });
+
+		return {
+			props: {
+				query,
+				...additional?.props,
+				dehydratedState: dehydrate(queryClient),
+			},
+		};
+	};
+};
