9edc4080925b2f3d52e98466c87d8c81d0bf7fb3
ilhamnrfdn <ilhamnoer@femaledaily.com>
2024-11-10 23:23:46 +0700

    add default queries for ssr

---
+++ frontend/src/hooks/default.queries.ts
@@ -0,0 +1,10 @@
+import { DefaultFetcher } from "@/utils/fetcher";
+
+export const defaultQuery = (params: any) => ({
+	queryFn: async (payload: any) =>
+		DefaultFetcher({
+			...payload,
+			url: params?.url,
+			method: "GET",
+		}),
+});
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,34 +1,39 @@
 import { Inter } from "next/font/google";
-import { List } from "antd";
 import dynamic from "next/dynamic";
+
+// utils
+import { ssrWrapper } from "@/Utils/ssrWrapper";
+import { GetServerSideProps } from "next";
+import { useQuery } from "@tanstack/react-query";
 
 // component
 const Card = dynamic(() => import("@/components/atom/card"), { ssr: false });
 
 const inter = Inter({ subsets: ["latin"] });
 
+interface IData {
+	status?: string;
+	data?: {
+		completed: boolean;
+		_id: string;
+		name: string;
+		content: string;
+		__v: number;
+	}[];
+}
 const Index = () => {
-	const data = [
-		{
-			title: "Title 1",
-		},
-		{
-			title: "Title 2",
-		},
-		{
-			title: "Title 3",
-		},
-		{
-			title: "Title 4",
-		},
-		{
-			title: "Title 5",
-		},
-		{
-			title: "Title 6",
-		},
-	];
+	const { data: dataTodo } = useQuery<IData>({
+		queryKey: [
+			"/todo",
+			{
+				page: 1,
+				limit: 10,
+			},
+			"v1",
+		],
+	});
 
+	console.log(dataTodo?.data);
 	return (
 		<>
 			<main
@@ -42,30 +47,13 @@ const Index = () => {
 			>
 				<div className="text-center mb-4 title">MY TODO LIST</div>
 				<div className="grid gap-4">
-					<Card
-						title="a"
-						content="bera"
-					/>
-					<Card
-						title="a"
-						content="bera"
-					/>
-					<Card
-						title="a"
-						content="bera"
-					/>
-					<Card
-						title="a"
-						content="bera"
-					/>
-					<Card
-						title="a"
-						content="bera"
-					/>
-					<Card
-						title="a"
-						content="bera"
-					/>
+					{dataTodo?.data?.map((data: any) => (
+						<Card
+							id={data?._id}
+							title={data.name}
+							content={data.content}
+						/>
+					))}
 				</div>
 			</main>
 			<style>
@@ -81,5 +69,30 @@ const Index = () => {
 		</>
 	);
 };
+
+export const getServerSideProps: GetServerSideProps = ssrWrapper(
+	async (context: any) => {
+		const { queryClient } = context;
+		// for query Key berisi url, maupun params sebagai api
+		const KEY = [
+			"/todo",
+			{
+				page: 1,
+				limit: 10,
+			},
+			"v1",
+		];
+
+		await queryClient.prefetchQuery({ queryKey: KEY });
+
+		console.log("hasil prefetc:", context);
+		return {
+			props: {
+				test: "uye",
+			},
+		};
+	}
+	// "/api/test"
+);
 
 export default Index;
