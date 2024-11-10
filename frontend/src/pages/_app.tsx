998e8de709564fb4e5307a4754d2e733fff5f890
ilhamnrfdn <ilhamnoer@femaledaily.com>
2024-11-10 23:23:03 +0700

    add hydration in core app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 // import "antd/dist/reset.css"; // Ant Design versi 5
 // import { useEffect } from "react";
 import {
+	HydrationBoundary,
 	QueryClient,
 	QueryClientProvider,
 	QueryErrorResetBoundary,
@@ -15,8 +16,11 @@ import { ErrorBoundary } from "react-error-boundary";
 // template component
 import FallbackComponent from "@/components/template/fallbackComponent";
 import Custom404 from "@/pages/404";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+	const { dehydratedState, test, ...restProp } = pageProps;
+	console.log("pageprops:", pageProps);
 	//   useEffect(() => {
 	// 		/**
 	// 		 * resgister this service worker in app because of
@@ -31,26 +35,48 @@ export default function App({ Component, pageProps }: AppProps) {
 	//         });
 	// 		}
 	// 	}, []);
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				retry: 0,
-			},
-		},
-	});
+
+	// NEVER DO THIS:
+	// const queryClient = new QueryClient()
+	//
+	// Creating the queryClient at the file root level makes the cache shared
+	// between all requests and means _all_ data gets passed to _all_ users.
+	// Besides being bad for performance, this also leaks any sensitive data.
+
+	// Instead do this, which ensures each request has its own cache:
+	const [queryClient] = React.useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						// With SSR, we usually want to set some default staleTime
+						// above 0 to avoid refetching immediately on the client
+						staleTime: 60 * 1000,
+						retry: 0,
+					},
+				},
+			})
+	);
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<QueryErrorResetBoundary>
-				{({ reset }) => (
-					<ErrorBoundary
-						onReset={reset}
-						FallbackComponent={FallbackComponent}
-					>
-						<Component {...pageProps} />
-					</ErrorBoundary>
-				)}
-			</QueryErrorResetBoundary>
+			{/* fungsi dari hydration yaitu menampilkan data secepatnya dari server ke client
+				hyrate bekerja apabila sebagai preload/re render dalam menampilkan data terlebih dahulu sebelum html atau js
+				saat menggunakan hydrate untuk ssr di mana kita bisa invoke setiap useQuery hook kedalam nest komponent yg lebih efektif
+				dan disaat pemanggilan query di client, maka klien tidak perlu lagi untuk fetching ulang data yg sudah terhydrate sebelumnya
+			*/}
+			<HydrationBoundary state={dehydratedState}>
+				<QueryErrorResetBoundary>
+					{({ reset }) => (
+						<ErrorBoundary
+							onReset={reset}
+							FallbackComponent={FallbackComponent}
+						>
+							<Component {...restProp} />
+						</ErrorBoundary>
+					)}
+				</QueryErrorResetBoundary>
+			</HydrationBoundary>
 			<ReactQueryDevtools initialIsOpen />
 		</QueryClientProvider>
 	);
